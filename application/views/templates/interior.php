0a035888d4877e09b658497cbbe9ed0e71993597
ksenia <ksy184694@gmail.com>
2024-04-19 12:04:38 +0300

    adding interier styles

--- application/views/templates/interior.php
+++ application/views/templates/interior.php
@@ -3,27 +3,33 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
 
 <!-- Замените 'path_to_your_interior_image' на путь к вашему изображению интерьера -->
-<section id="interior" style="background-image: url('path_to_your_interior_image');">
-    <div class="container">
+<section id="interior">
+    <div class="background-overlay"></div> <!-- Новый элемент для фона -->
+    <div id="interior-header">
         <h2>Интерьер</h2>
         <h3>Наши фотографии</h3>
     </div>
 </section>
 
-<section id="photos">
-    <div class="container">
-        <!-- Здесь вставьте фотографии интерьера -->
+    <section id="photos">
         <div class="photo-row">
             <div class="photo-item">
-                <img src="path_to_your_photo1" alt="Photo 1">
+                <img src="<?php echo base_url('assets/img/interier1.jpg'); ?>" alt="Photo 1">
             </div>
             <div class="photo-item">
-                <img src="path_to_your_photo2" alt="Photo 2">
+                <img src="<?php echo base_url('assets/img/interier2.jpg'); ?>" alt="Photo 2">
             </div>
-            <!-- Продолжайте добавлять фотографии в таком же формате -->
+            <div class="photo-item">
+                <img src="<?php echo base_url('assets/img/interier3.jpg'); ?>" alt="Photo 1">
+            </div>
+            <div class="photo-item">
+                <img src="<?php echo base_url('assets/img/interier4.jpg'); ?>" alt="Photo 1">
+            </div>
+            <div class="photo-item">
+                <img src="<?php echo base_url('assets/img/interier5.jpg'); ?>" alt="Photo 1">
+            </div>
+            <!-- Добавьте остальные фотографии здесь -->
         </div>
-        <!-- Продолжайте добавлять ряды с фотографиями по вашему желанию -->
-    </div>
-</section>
+    </section>
 
 <?php include('templates/footer.php'); ?>
